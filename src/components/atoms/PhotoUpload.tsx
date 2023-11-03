e558f6b7c1590c6bd9b2263168181e23a35e9352
Ahsan Amin <ahsan.amin@headless.ltd>
2023-11-03 18:35:34 +0600

    chore

--- src/components/atoms/PhotoUpload.tsx
+++ src/components/atoms/PhotoUpload.tsx
@@ -10,6 +10,22 @@ const StyledInput = styled.label`
   border: 2px dotted #c3c3c7;
   border-radius: 5px;
   cursor: pointer;
+
+  height: 240px;
+  width: 240px;
+
+  @media only screen and (min-width: 1440px) and (max-width: 1600px) {
+    height: 200px;
+    width: 200px;
+  }
+  @media only screen and (min-width: 1280px) and (max-width: 1400px) {
+    height: 160px;
+    width: 160px;
+  }
+  @media only screen and (max-width: 1200px) {
+    height: 100px;
+    width: 100px;
+  }
 `;
 
 const PhotoUpload: React.FC = () => {
